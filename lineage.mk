be8ce5c6704bc399f7e83759fc2f66ab6db5052b
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-02 13:03:06 -0600

    v480: cm->lineage
    Change-Id: I04b8c553ef7674cf7354bcfe64a073da37c16984

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/lge/v480/full_v480.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_v480
+PRODUCT_NAME := lineage_v480
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="v480" \
